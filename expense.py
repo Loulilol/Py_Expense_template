51e9f2039ae91def58e336e4a45c0180c394dcf0
Lou Lefebvre <lou.lefebvre@epita.fr>
2023-09-20 21:21:15 +0200

    ADD files expenses and users

--- expense.py
+++ expense.py
@@ -1,4 +1,27 @@
 from PyInquirer import prompt
+import csv
+
+def get_users():
+    users = []
+    with open('users.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if row:
+                user = row[0]
+                users.append(user)
+    print(users)
+    return users
+
+def get_involved():
+    involved = []
+    with open('users.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if row:
+                inv = row[0]
+                involved.append({ 'name' : inv })
+    print(involved)
+    return involved
 
 expense_questions = [
     {
@@ -12,19 +35,26 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": get_users()
     },
-
+    {
+        "type":"checkbox",
+        "name":"involved",
+        "message":"New Expense - Involved: ",
+        "choices": get_involved()
+    },
 ]
-
-
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+    #if not isinstance(infos['amount'], int):
+    #    raise Exception("Expense Amount is not a number")
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    with open("expense_report.csv", 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([infos['amount'], infos['label'], infos['spender'], infos['involved']])
     print("Expense Added !")
     return True
-
-
--- user.py
+++ user.py
@@ -1,8 +1,19 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+     {
+        "type":"input",
+        "name":"name",
+        "message":"What is your name ? ",
+    },
 ]
 
-def add_user():
+def add_user(*args):
     # This function should create a new user, asking for its name
-    return
+    newUser = prompt(user_questions)
+    with open("users.csv", 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([newUser['name']])
+    print("User Added !")
+    return True
